51f8484507d5425f9f56432d2556c41d936a4af7
geekwhocodes <ganeshraskar@outlook.com>
2020-07-25 00:14:04 +0530

    Add script to mount file share

---
+++ mount-file-share.ps1
@@ -0,0 +1,45 @@
+#!/bin/pwsh
+
+$AZURE_STORAGE_ACCOUNT = "storageaccounthuggib054"
+$resource_group = "huggingface"
+$functionAppName = "gwc-hugging"
+$shareName = "models"
+$directoryName = "models"
+$shareId = "gwc$(Get-Random -Count 1)"
+$mountPath = "/models"
+
+#Write-Progress "Get Storage account key" "Inprogress"
+$AZURE_STORAGE_KEY = $(az storage account keys list -g $resource_group -n $AZURE_STORAGE_ACCOUNT --query '[0].value' -o tsv)
+#Write-Progress "Get Storage account key" "Complete"
+
+#Write-Progress "Create file share $shareName" "Inprogress"
+az storage share create --account-name $AZURE_STORAGE_ACCOUNT `
+  --account-key $AZURE_STORAGE_KEY `
+  --name $shareName `
+  --quota 50
+#Write-Progress "Create file share $shareName" "Complete"
+
+# #Write-Progress "Create directory $shareName" "Inprogress"
+# az storage directory create --account-name $AZURE_STORAGE_ACCOUNT `
+#   --account-key $AZURE_STORAGE_KEY `
+#   --share-name $shareName `
+#   --name $directoryName
+# #Write-Progress "Create directory $shareName" "Complete"
+
+#Write-Progress "Mount share to web app" "Inprogress"
+az webapp config storage-account add `
+  --resource-group $resource_group `
+  --name $functionAppName `
+  --custom-id $shareId `
+  --storage-type "AzureFiles" `
+  --share-name $shareName `
+  --account-name $AZURE_STORAGE_ACCOUNT `
+  --mount-path $mountPath `
+  --access-key $AZURE_STORAGE_KEY
+#Write-Progress "Mount share to web app" "Complete"
+
+
+
+# az storage file upload-batch --account-key $AZURE_STORAGE_KEY `
+#   --account-name $AZURE_STORAGE_ACCOUNT --destination $directoryName `
+#   --source ./models
